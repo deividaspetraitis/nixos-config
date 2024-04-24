62b3e8ca0978a9fea72b5db8f9cf4e4fc0a1498f
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-04-24 09:06:43 +0300

    Remove Obsidian Electron override
    Fixed in: https://github.com/NixOS/nixpkgs/issues/273611

--- users/deividas/home.nix
+++ users/deividas/home.nix
@@ -17,7 +17,6 @@
 
   # Permitted list of insecure packages.
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
 
   # Enable flakes
   nix = {
