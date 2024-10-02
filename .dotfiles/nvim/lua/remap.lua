1220bd6f4f0b3c54f3a46591a67ebfbb7d370982
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-10-02 11:40:11 +0300

    nvim: terminal navigation keys remap

--- .dotfiles/nvim/lua/remap.lua
+++ .dotfiles/nvim/lua/remap.lua
@@ -17,3 +17,9 @@ vim.api.nvim_set_keymap('n', '[l', ':lprevious<CR>', { noremap = true, silent = 
 
 -- Quick write shortcut
 vim.api.nvim_set_keymap('n', '<localleader>w', ':w<CR>', { noremap = true, silent = true })
+--
+-- Remap <C-w> navigation keys in terminal mode
+vim.api.nvim_set_keymap('t', '<C-w>h', '<C-\\><C-n><C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-w>j', '<C-\\><C-n><C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-w>k', '<C-\\><C-n><C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-w>l', '<C-\\><C-n><C-w>l', { noremap = true, silent = true })
