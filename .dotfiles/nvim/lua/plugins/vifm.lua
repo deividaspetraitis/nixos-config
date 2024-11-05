50837fcb14aef6872ffe8fe98477f0d594499df9
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-11-05 08:50:20 +0200

    nvim: vifm

---
+++ .dotfiles/nvim/lua/plugins/vifm.lua
@@ -0,0 +1,11 @@
+return {
+	"vifm/vifm.vim",
+	tag = "v0.13",
+	config = function()
+		-- Use Vifm as the default file explorer
+		vim.g.vifm_replace_netrw = 1
+
+		-- Vifm leader mapping
+		vim.api.nvim_set_keymap('n', '<leader>m', ':FloatermNew vifm<CR>', { noremap = true, silent = true })
+	end
+}
