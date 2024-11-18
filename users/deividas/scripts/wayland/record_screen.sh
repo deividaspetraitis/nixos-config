210a5ff0a64a116abb8936eae899bbe3bca77986
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-11-18 10:40:01 +0200

    Screen recording script: handle stop recording

--- users/deividas/scripts/wayland/record_screen.sh
+++ users/deividas/scripts/wayland/record_screen.sh
@@ -4,6 +4,7 @@
 NAME=$(date +'recording_%Y-%m-%d-%H%M%S.png')
 VIDEO_FILE="/tmp/$NAME.mp4"
 GIF_FILE="$HOME/Documents/$NAME.gif"
+STATUS_FILE="/tmp/wf-recorder-status"
 
 # Ensure required tools are installed
 if ! command -v wf-recorder &> /dev/null || ! command -v slurp &> /dev/null || ! command -v wl-copy &> /dev/null || ! command -v ffmpeg &> /dev/null; then
@@ -11,12 +12,23 @@ if ! command -v wf-recorder &> /dev/null || ! command -v slurp &> /dev/null || !
     exit 1
 fi
 
+# If the file exists, stop the recording and exit
+# Otherwise, start recording
+if [ -f $STATUS_FILE ]; then
+    pkill wf-recorder
+    rm $STATUS_FILE
+	exit 0 # exit successfully, to continue execution of original script
+fi
+
 # Select an area with slurp
 GEOMETRY=$(slurp)
 if [ -z "$GEOMETRY" ]; then
-    notify-send "Recording canceled" "No area was selected."
-    exit 1
+	notify-send "Recording canceled" "No area was selected."
+	exit 1
 fi
+
+# Create a status file to indicate that recording has started
+touch $STATUS_FILE
 
 # Notify the user and start recording
 notify-send "Recording started"
