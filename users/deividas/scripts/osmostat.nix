43666247c43d3cd491785c07cb87b7cb91e62f9e
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-03-01 12:49:25 +0200

    home: add support of accepting what keys osmostat script should report

--- users/deividas/scripts/osmostat.nix
+++ users/deividas/scripts/osmostat.nix
@@ -1,5 +1,11 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "osmostat" ''
-  ${pkgs.curl}/bin/curl "https://rpc.osmosis.zone/status" -s | ${pkgs.jq}/bin/jq
+  ${pkgs.curl}/bin/curl "https://rpc.osmosis.zone/status" -s | ${pkgs.jq}/bin/jq --arg key "$@" '
+    . as $orig |  # Keep the original JSON
+    reduce paths(scalars) as $p ({}; 
+      if ($p[-1] | test($key)) then 
+        setpath($p; $orig | getpath($p)) 
+      else . end
+    )'
 ''
