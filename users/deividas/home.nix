6d2e469a14c5ac97c766e29e144ee4661e6acea6
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-05-17 16:36:32 +0300

    home: `programs.zsh.initExtra` is deprecated, use `programs.zsh.initContent`

--- users/deividas/home.nix
+++ users/deividas/home.nix
@@ -58,12 +58,12 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-	(import ./scripts/osmostat.nix { inherit pkgs; })
-	(import ./scripts/osmodenomliq.nix { inherit pkgs; })
-	(import ./scripts/osmodenom.nix { inherit pkgs; })
+    (import ./scripts/osmostat.nix { inherit pkgs; })
+    (import ./scripts/osmodenomliq.nix { inherit pkgs; })
+    (import ./scripts/osmodenom.nix { inherit pkgs; })
 
     # Adjusts your screen to emit warmer light based on the time of day
-	pkgs.openssl
+    pkgs.openssl
     pkgs.gnumake
     pkgs.gcc
     pkgs.act
@@ -74,7 +74,7 @@
     pkgs.nix-prefetch-github
     pkgs.wrk
     pkgs.jq
-	pkgs.yq
+    pkgs.yq
     pkgs.ripgrep
     pkgs.obsidian
     pkgs.chromium
@@ -90,7 +90,7 @@
     pkgs.k9s
     pkgs.direnv
     pkgs.datadog-agent
-	pkgs.harlequin
+    pkgs.harlequin
 
     # Lmstudio
     pkgs.lmstudio
@@ -334,79 +334,79 @@
     # Whether to enable Z Shell or not.
     enable = true;
 
-    initExtra = ''
-      # Options
-      # More options: https://zsh.sourceforge.io/Doc/Release/Options.html
-      setopt AUTO_PARAM_SLASH
-      unsetopt CASE_GLOB
-      
-      setopt AUTO_PUSHD
-      setopt PUSHD_IGNORE_DUPS
-      setopt PUSHD_SILENT
+    initContent = ''
+       # Options
+       # More options: https://zsh.sourceforge.io/Doc/Release/Options.html
+       setopt AUTO_PARAM_SLASH
+       unsetopt CASE_GLOB
 
-      # History
-      # http://zsh.sourceforge.net/Doc/Release/Options.html#History
-      setopt append_history          # append to history file
-      setopt extended_history        # write the history file in the ':start:elapsed;command' format
-      unsetopt hist_beep             # don't beep when attempting to access a missing history entry
-      setopt hist_find_no_dups       # don't display a previously found event
-      setopt hist_ignore_all_dups    # delete an old recorded event if a new event is a duplicate
-      setopt hist_ignore_dups        # don't record an event that was just recorded again
-      setopt hist_ignore_space       # don't record an event starting with a space
-      setopt hist_no_store           # don't store history commands
-      setopt hist_reduce_blanks      # remove superfluous blanks from each command line being added to the history list
-      setopt hist_save_no_dups       # don't write a duplicate event to the history file
-      setopt hist_verify             # don't execute immediately upon history expansion
-      setopt inc_append_history      # write to the history file immediately, not when the shell exits
-      setopt share_history           # share history between all sessions
+       setopt AUTO_PUSHD
+       setopt PUSHD_IGNORE_DUPS
+       setopt PUSHD_SILENT
 
-      # Enable extended globbing
-      setopt EXTENDED_GLOB
+       # History
+       # http://zsh.sourceforge.net/Doc/Release/Options.html#History
+       setopt append_history          # append to history file
+       setopt extended_history        # write the history file in the ':start:elapsed;command' format
+       unsetopt hist_beep             # don't beep when attempting to access a missing history entry
+       setopt hist_find_no_dups       # don't display a previously found event
+       setopt hist_ignore_all_dups    # delete an old recorded event if a new event is a duplicate
+       setopt hist_ignore_dups        # don't record an event that was just recorded again
+       setopt hist_ignore_space       # don't record an event starting with a space
+       setopt hist_no_store           # don't store history commands
+       setopt hist_reduce_blanks      # remove superfluous blanks from each command line being added to the history list
+       setopt hist_save_no_dups       # don't write a duplicate event to the history file
+       setopt hist_verify             # don't execute immediately upon history expansion
+       setopt inc_append_history      # write to the history file immediately, not when the shell exits
+       setopt share_history           # share history between all sessions
 
-      # Enable Vi mode
-      bindkey -v
-      bindkey "^?" backward-delete-char # Fix modes N -> I -> I backspace  not working
-      
-      export KEYTIMEOUT=1 # timeout for switching between INSERT/NORMAL modes
-      
-      # Enable CTRL-P CTRL-N
-      bindkey "^P" up-line-or-search
-      bindkey "^N" down-line-or-search
+       # Enable extended globbing
+       setopt EXTENDED_GLOB
 
-	  # Open editor with CTRL-X
-      open_editor() {
-          $EDITOR "$@"
-      }
-      
-      zle -N open_editor  # Register as a Zsh widget
-      bindkey "^X" open_editor  # Bind Ctrl+X
-      
-      # By default zsh yanks to its own internal registers.
-      # Luckily, like in Vim, it's fairly simple yank to the system clipboard.
-      function vi-yank-clip {
-          zle vi-yank
-          echo "$CUTBUFFER" | wl-copy # wl-copy is OS specific
-      }
-      zle -N vi-yank-clip
-      bindkey -M vicmd 'y' vi-yank-clip
+       # Enable Vi mode
+       bindkey -v
+       bindkey "^?" backward-delete-char # Fix modes N -> I -> I backspace  not working
 
-      # Edit command line in Vim
-      autoload -Uz edit-command-line
-      zle -N edit-command-line
-      bindkey -M vicmd v edit-command-line
+       export KEYTIMEOUT=1 # timeout for switching between INSERT/NORMAL modes
 
-      # Autocomplete hidden files
-      _comp_options+=(globdots)
+       # Enable CTRL-P CTRL-N
+       bindkey "^P" up-line-or-search
+       bindkey "^N" down-line-or-search
 
-      # Start tmux on every shell login
-      if pgrep --exact sway > /dev/null 2>&1 && [[ -z "$TMUX" ]]; then
-          tmux attach || tmux new-session
-      fi
+      # Open editor with CTRL-X
+       open_editor() {
+           $EDITOR "$@"
+       }
 
-      # add ~/.config/zsh/external to fpath, and then lazy autoload
-      # every file in there as a function
-      fpath=(~/.config/zsh/external $fpath);
-      autoload -U $fpath[1]/*(-.:t)
+       zle -N open_editor  # Register as a Zsh widget
+       bindkey "^X" open_editor  # Bind Ctrl+X
+
+       # By default zsh yanks to its own internal registers.
+       # Luckily, like in Vim, it's fairly simple yank to the system clipboard.
+       function vi-yank-clip {
+           zle vi-yank
+           echo "$CUTBUFFER" | wl-copy # wl-copy is OS specific
+       }
+       zle -N vi-yank-clip
+       bindkey -M vicmd 'y' vi-yank-clip
+
+       # Edit command line in Vim
+       autoload -Uz edit-command-line
+       zle -N edit-command-line
+       bindkey -M vicmd v edit-command-line
+
+       # Autocomplete hidden files
+       _comp_options+=(globdots)
+
+       # Start tmux on every shell login
+       if pgrep --exact sway > /dev/null 2>&1 && [[ -z "$TMUX" ]]; then
+           tmux attach || tmux new-session
+       fi
+
+       # add ~/.config/zsh/external to fpath, and then lazy autoload
+       # every file in there as a function
+       fpath=(~/.config/zsh/external $fpath);
+       autoload -U $fpath[1]/*(-.:t)
     '';
 
     shellAliases = {
