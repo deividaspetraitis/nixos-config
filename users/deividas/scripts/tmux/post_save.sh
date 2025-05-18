690d62a0718d708504ce13cbfb2112c85e8b2596
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-05-18 19:52:36 +0300

    tmux: post_save.sh script update

--- users/deividas/scripts/tmux/post_save.sh
+++ users/deividas/scripts/tmux/post_save.sh
@@ -1,3 +1,2 @@
-sed -ie "s| --cmd .*-vim-pack-dir||g" $1
-sed -i "s|/nix/store/\(.*\)/bin/||g" $1
+sed -i "s|/nix/store[^ ]*/bin/nvim --cmd[^ ]* [^ ]* [^ ]*|nvim|" $1
 sed -i "s|/run/current-system/sw/bin/||g" $1
