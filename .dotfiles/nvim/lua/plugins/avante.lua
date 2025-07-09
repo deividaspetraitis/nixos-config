07ea1eef667a45ab7774bb1400997e20c5463f7b
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-07-09 13:21:43 +0300

    nvim: update avante plugin

--- .dotfiles/nvim/lua/plugins/avante.lua
+++ .dotfiles/nvim/lua/plugins/avante.lua
@@ -5,32 +5,45 @@ return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
 	lazy = false,
-	tag = "v0.0.21",
+	-- tag = "v0.0.25",
+	--
+	version = false,                    -- Never set this value to "*"! Never!
 	opts = {
-		-- add any opts here
+		auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+		provider = "claude",            -- Recommend using Claude
+		providers = {
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-sonnet-4-20250514",
+				timeout = 30000, -- Timeout in milliseconds
+				extra_request_body = {
+					temperature = 0.75,
+					max_tokens = 20480,
+				},
+			},
+		},
+		behaviour = {
+			auto_suggestions = false, -- Experimental stage
+		},
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-	build = "make",
+	-- ⚠️ must add this setting! ! !
+	build = function()
+		-- conditionally use the correct build system for the current OS
+		if vim.fn.has("win32") == 1 then
+			return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+		else
+			return "make"
+		end
+	end,
 	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 	dependencies = {
 		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 	},
-
 	config = function()
 		require('avante').setup {
-			provider = "claude",         -- Recommend using Claude
-			auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-			claude = {
-				endpoint = "https://api.anthropic.com",
-				model = "claude-3-5-sonnet-20240620",
-				temperature = 0,
-				max_tokens = 4096,
-			},
-			behaviour = {
-				auto_suggestions = false, -- Experimental stage
-			},
 			mappings = {
 				submit = {
 					normal = "<CR>",
