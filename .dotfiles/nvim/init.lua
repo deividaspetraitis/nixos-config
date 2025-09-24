e75be5149d67fa89c3c7836c0858762948b3435f
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-09-24 17:30:33 +0300

    nvim: auto format

--- .dotfiles/nvim/init.lua
+++ .dotfiles/nvim/init.lua
@@ -3,10 +3,10 @@ require("remap")
 require("config.lazy")
 
 vim.api.nvim_create_autocmd("VimResume", {
-  callback = function()
-    vim.cmd("mode")
-  end,
-  desc = "Fix terminal redraw issues when returning to Neovim caused by hrsh7th/nvim-cmp in tmux terminal",
+	callback = function()
+		vim.cmd("mode")
+	end,
+	desc = "Fix terminal redraw issues when returning to Neovim caused by hrsh7th/nvim-cmp in tmux terminal",
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -35,12 +35,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		opts.desc = "Show LSP type definitions"
 		vim.keymap.set("n", "<localleader>gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-		opts.desc = "See available code actions"
-		vim.keymap.set({ "n", "v" }, "<localleader>q", vim.lsp.buf.code_action, opts)
-
-		opts.desc = "Smart rename"
-		vim.keymap.set("n", "<localleader>r", vim.lsp.buf.rename, opts)
-
 		opts.desc = "Show buffer diagnostics"
 		vim.keymap.set("n", "<localleader>d", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
@@ -61,57 +55,84 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+-- Auto-format files before saving
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		local clients = vim.lsp.get_clients({ bufnr = 0 })
+		if #clients > 0 then
+			local workspaces = vim.lsp.buf.list_workspace_folders()
+			-- there could be multiple workspace folders
+			-- keep it simple for now and just check the first one
+			local ws = workspaces[1]
+			if ws then
+				local noformat = vim.fs.find(".noformat", {
+					path = ws,
+					upward = false, -- only check inside workspace root
+					type = "file",
+				})[1]
+
+				if noformat then
+					return -- skip formatting
+				end
+			end
+
+			vim.lsp.buf.format({ async = false })
+		end
+	end,
+})
+
 -- If you don't want to turn 'hlsearch' on, but want to highlight all matches while searching
 vim.api.nvim_create_augroup("vimrc-incsearch-highlight", { clear = true })
-vim.api.nvim_create_autocmd({"CmdlineEnter"}, {
-  group = "vimrc-incsearch-highlight",
-  pattern = {"/", "\\?"},
-  command = "set hlsearch"
+vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
+	group = "vimrc-incsearch-highlight",
+	pattern = { "/", "\\?" },
+	command = "set hlsearch"
 })
-vim.api.nvim_create_autocmd({"CmdlineLeave"}, {
-  group = "vimrc-incsearch-highlight",
-  pattern = {"/", "\\?"},
-  command = "set nohlsearch"
+vim.api.nvim_create_autocmd({ "CmdlineLeave" }, {
+	group = "vimrc-incsearch-highlight",
+	pattern = { "/", "\\?" },
+	command = "set nohlsearch"
 })
 
 -- Cursor line settings: Display cursor line only in an active window.
 vim.api.nvim_create_augroup("CursorLineOnlyInActiveWindow", { clear = true })
-vim.api.nvim_create_autocmd({"VimEnter", "WinEnter", "BufWinEnter"}, {
-  group = "CursorLineOnlyInActiveWindow",
-  command = "setlocal cursorline"
+vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter" }, {
+	group = "CursorLineOnlyInActiveWindow",
+	command = "setlocal cursorline"
 })
 vim.api.nvim_create_autocmd("WinLeave", {
-  group = "CursorLineOnlyInActiveWindow",
-  command = "setlocal nocursorline"
+	group = "CursorLineOnlyInActiveWindow",
+	command = "setlocal nocursorline"
 })
 
 -- Return to last edit position when opening files (You want this!)
 vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-      vim.cmd("normal! g`\"")
-    end
-  end
+	pattern = "*",
+	callback = function()
+		if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			vim.cmd("normal! g`\"")
+		end
+	end
 })
 
 -- Quite a few people accidentally type "q:" instead of ":q" and get confused
 -- by the command line window. Give a hint about how to get out.
 vim.api.nvim_create_augroup("vimHints", { clear = true })
 vim.api.nvim_create_autocmd("CmdwinEnter", {
-  group = "vimHints",
-  command = [[echohl Todo | echo 'You discovered the command-line window! You can close it with ":q".' | echohl None]]
+	group = "vimHints",
+	command = [[echohl Todo | echo 'You discovered the command-line window! You can close it with ":q".' | echohl None]]
 })
 
 -- Convenient command to see the difference between the current buffer and the file it was loaded from
 if not vim.fn.exists(":DiffOrig") then
-  vim.api.nvim_create_user_command('DiffOrig', function()
-    vim.cmd('vert new')
-    vim.cmd('set bt=nofile')
-    vim.cmd('r ++edit #')
-    vim.cmd('0d_')
-    vim.cmd('diffthis')
-    vim.cmd('wincmd p')
-    vim.cmd('diffthis')
-  end, {})
+	vim.api.nvim_create_user_command('DiffOrig', function()
+		vim.cmd('vert new')
+		vim.cmd('set bt=nofile')
+		vim.cmd('r ++edit #')
+		vim.cmd('0d_')
+		vim.cmd('diffthis')
+		vim.cmd('wincmd p')
+		vim.cmd('diffthis')
+	end, {})
 end
