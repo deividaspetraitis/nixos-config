df0c11b6ac93f353d586ea822b85f5e849d446a2
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-09-25 20:14:53 +0300

    nvim: bump LSP config

--- .dotfiles/nvim/lua/plugins/lsp.lua
+++ .dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,34 +1,13 @@
--- Data only repo providing basic, default Nvim LSP client configurations for various LSP servers.
--- https://github.com/neovim/nvim-lspconfig
 return {
-	"neovim/nvim-lspconfig",
-	tag = "v2.4.0",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"j-hui/fidget.nvim",
-	},
-
-	config = function()
-		local cmp = require('cmp')
-		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities())
-
-		require("fidget").setup({})
-		require("mason").setup()
-
-		require("mason-lspconfig").setup({
+	{
+		-- Automatically install and enable LSP servers
+		-- https://github.com/mason-org/mason-lspconfig.nvim
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			{ "j-hui/fidget.nvim",    opts = {} },
+		},
+		opts = {
 			ensure_installed = {
 				'gopls',
 				'lua_ls',
@@ -40,111 +19,140 @@ return {
 				'bashls',
 				'yamlls',
 			},
-			handlers = {
-				function(server_name) -- default handler (optional)
-					vim.lsp.config[server_name].setup {
-						capabilities = capabilities
-					}
+		},
+	},
+	{
+		-- Data only repo providing basic, default Nvim LSP client configurations for various LSP servers.
+		-- https://github.com/neovim/nvim-lspconfig
+		"neovim/nvim-lspconfig",
+		config = function(_, opts)
+			local cmp_lsp = require("cmp_nvim_lsp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				cmp_lsp.default_capabilities()
+			)
+
+			vim.lsp.config("gopls", {
+				capabilities = capabilities,
+				settings = {
+					gopls = {
+						analyses = {
+							unusedparams = true,
+							modernize = true,
+						},
+						staticcheck = true,
+						gofumpt = true,
+					},
+				},
+			})
+
+			vim.lsp.config("yamlls", {
+				on_attach = function(client, bufnr)
+					client.server_capabilities.documentFormattingProvider = true
 				end,
-				["gopls"] = function()
-					vim.lsp.config.gopls.setup {
-						capabilities = capabilities,
-						settings = {
-							gopls = {
-								analyses = {
-									unusedparams = true,
-									modernize = true,
-								},
-								staticcheck = true,
-								gofumpt = true,
+				capabilities = capabilities,
+				settings = {
+					yaml = {
+						format = {
+							enable = true
+						},
+						schemaStore = {
+							enable = true
+						}
+					}
+				}
+			})
+
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using (most
+							-- likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT',
+							-- Tell the language server how to find Lua modules same way as Neovim
+							-- (see `:h lua-module-load`)
+							path = {
+								'lua/?.lua',
+								'lua/?/init.lua',
 							},
 						},
-					}
-				end,
-				["yamlls"] = function()
-					vim.lsp.config.yamlls.setup {
-						on_attach = function(client, bufnr)
-							client.server_capabilities.documentFormattingProvider = true
-						end,
-						flags = lsp_flags,
-						capabilities = capabilities,
-						settings = {
-							yaml = {
-								format = {
-									enable = true
-								},
-								schemaStore = {
-									enable = true
-								}
-							}
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME
+								-- Depending on the usage, you might want to add additional paths
+								-- here.
+								-- '${3rd}/luv/library'
+								-- '${3rd}/busted/library'
+							},
+						},
+						diagnostics = {
+							globals = { "vim" },
 						}
 					}
-				end,
-				["lua_ls"] = function()
-					vim.lsp.config.lua_ls.setup {
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								runtime = { version = "Lua 5.1" },
-								diagnostics = {
-									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-								}
-							}
-						}
-					}
-				end,
-			}
-		})
-
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-		local luasnip = require("luasnip")
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				['<C-y>'] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-				['<C-n>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item(cmp_select)
-					elseif luasnip.locally_jumpable(1) then
-						luasnip.jump(1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				['<C-p>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item(cmp_select)
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' }, -- For luasnip users.
-			}, {
-				{ name = 'buffer' },
+				}
 			})
-		})
 
-		vim.diagnostic.config({
-			-- update_in_insert = true,
-			float = {
-				focusable = false,
-				style = "minimal",
-				border = "rounded",
-				source = "always",
-				header = "",
-				prefix = "",
-			},
-		})
-	end
+			require("mason-lspconfig").setup(opts)
+		end
+	},
+	{
+		-- A completion engine plugin
+		-- https://github.com/hrsh7th/nvim-cmp
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require('cmp')
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+			local luasnip = require("luasnip")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-y>'] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+					['<C-n>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item(cmp_select)
+						elseif luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					['<C-p>'] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item(cmp_select)
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' }, -- For luasnip users.
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+		end
+	}
 }
