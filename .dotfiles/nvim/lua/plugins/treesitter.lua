31659099825eda0693a9c54a0754703390ed80c0
Deividas Petraitis <hi@deividaspetraitis.lt>
2026-05-28 15:27:39 +0300

    nvim: bump plugins

--- .dotfiles/nvim/lua/plugins/treesitter.lua
+++ .dotfiles/nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,11 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
 	"nvim-treesitter/nvim-treesitter",
-	tag = "v0.9.3",
+	tag = "v0.10.0",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
@@ -39,37 +40,41 @@ return {
 					set_jumps = true, -- whether to set jumps in the jumplist
 					goto_next_start = {
 						["]m"] = "@function.outer",
-						["]]"] = { query = "@class.outer", desc = "Next class start" },
-						--
-						-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-						["]o"] = "@loop.*",
-						-- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-						--
-						-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-						-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-						["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+						["]]"] = "@class.outer",
+						["]l"] = "@loop.outer",
 						["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
 					},
 					goto_next_end = {
 						["]M"] = "@function.outer",
 						["]["] = "@class.outer",
+						["]L"] = "@loop.outer",
 					},
 					goto_previous_start = {
 						["[m"] = "@function.outer",
 						["[["] = "@class.outer",
+						["[l"] = "@loop.outer",
 					},
 					goto_previous_end = {
 						["[M"] = "@function.outer",
 						["[]"] = "@class.outer",
+						["[L"] = "@loop.outer",
 					},
 					-- Below will go to either the start or the end, whichever is closer.
 					-- Use if you want more granular movements
 					-- Make it even more gradual by adding multiple queries and regex.
 					goto_next = {
-						["]d"] = "@conditional.outer",
+						["]a"] = "@assignment.outer",
+						["]#"] = "@conditional.outer",
+						["]p"] = "@parameter.outer",
+						["]s"] = "@statement.outer",
+						["]/"] = "@comment.outer",
 					},
 					goto_previous = {
-						["[d"] = "@conditional.outer",
+						["[a"] = "@assignment.outer",
+						["[#"] = "@conditional.outer",
+						["[p"] = "@parameter.outer",
+						["[s"] = "@statement.outer",
+						["[/"] = "@comment.outer",
 					}
 				},
 			},
